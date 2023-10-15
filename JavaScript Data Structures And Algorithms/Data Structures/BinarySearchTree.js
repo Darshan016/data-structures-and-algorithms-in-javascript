11ddae64d11a0ee6834964c15dbc3b109ab45a32
Darshan Panchal <panchaldarshan121@gmail.com>
2023-10-15 12:56:57 +0530

    implemented some traversal methods such as inroder, postorder, and BFS method such as levelorder and some helper methods such as min,max and delete

--- JavaScript Data Structures And Algorithms/Data Structures/BinarySearchTree.js
+++ JavaScript Data Structures And Algorithms/Data Structures/BinarySearchTree.js
@@ -61,6 +61,80 @@ class BinarySearchTree{
             this.preOrder(root.right)
         }
     }
+
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value)
+            this.inOrder(root.right)
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value)
+        }
+    }
+
+    levelOrder(){
+        const queue=[]
+        queue.push(this.root)
+        while(queue.length){
+            let curr=queue.shift()
+            console.log(curr.value)
+            if(curr.left){
+                queue.push(curr.left)
+            }
+            if(curr.right){
+                queue.push(curr.right)
+            }
+        }
+    }
+
+    min(root){
+        if(!root.left){
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+
+    max(root){
+        if(!root.right){
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
+
+    delete(value){
+        this.root=this.deleteNode(this.root,value)
+    }
+
+    deleteNode(root,value){
+        if(root===null){
+            return root
+        }
+        if(value < root.value){
+            root.left = this.deleteNode(root.left, value)
+        } else if(value > root.value){
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            if(!root.left && !root.right){
+                return null
+            }
+            if(!root.left){
+                return root.right
+            } else if(!root.right){
+                return root.left
+            }
+            root.value=this.min(root.right)
+            root.right=this.deleteNode(root.right,root.value)
+        }
+        return root
+    }
 }
 
 const tree=new BinarySearchTree()
@@ -73,4 +147,11 @@ tree.insert(3)
 tree.insert(7)
 // console.log(tree.isEmpty())
 // console.log(tree.search(tree.root,12))
-tree.preOrder(tree.root)
+// tree.preOrder(tree.root)
+// tree.inOrder(tree.root)
+// tree.postOrder(tree.root)
+tree.levelOrder()
+// console.log(tree.min(tree.root))
+// console.log(tree.max(tree.root))
+tree.delete(3)
+tree.levelOrder()
